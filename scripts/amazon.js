1a88d7bce8aac1e3c32d8e1c0dea50ec7510ed65
qquerellka <uskovmaksof@gmail.com>
2025-04-12 13:13:55 +0300

    add modules & generate html for cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,19 @@
+import { cart, addToCart } from '../data/cart.js';
+import { products } from '../data/products.js';
 let productsHTML = '';
+
+
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((item) => {
+    cartQuantity =+ item.quantity;
+  })
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
 
 products.forEach((product) => {
   productsHTML += `
@@ -53,34 +68,12 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
 
-
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if (item.productId === productId) {
-        matchingItem = item;
-      };
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId,
-        quantity: 1
-      })
-    };
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {
-      cartQuantity =+ item.quantity;
-    })
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    addToCart(productId);
+    updateCartQuantity();
   });
 });
