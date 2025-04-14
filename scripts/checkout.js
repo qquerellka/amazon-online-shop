645418bed0cde29c5b37d9990740723ccdf8bb92
qquerellka <uskovmaksof@gmail.com>
2025-04-14 16:46:29 +0300

    check

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,12 @@
 import { calculateCartQuantity, cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import dayjs from " https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(deliveryDate.format('dddd MMMM D'));
+console.log(deliveryDate);
+
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
